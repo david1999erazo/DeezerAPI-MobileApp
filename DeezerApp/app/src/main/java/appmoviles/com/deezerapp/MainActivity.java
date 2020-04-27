d3c2412bc47ebe7f1c90d7d82a48f406bf611cae
david1999erazo <david.erazo2@correo.icesi.edu.co>
2020-04-27 11:48:06 -0500

    Add structure

---
+++ DeezerApp/app/src/main/java/appmoviles/com/deezerapp/MainActivity.java
@@ -0,0 +1,14 @@
+package appmoviles.com.deezerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
